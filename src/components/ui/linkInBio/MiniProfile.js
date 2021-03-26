fbe75b89c2714e5b3a5317f3f52c4da17c7af2fd
bmumz <brandimummery@gmail.com>
2021-03-26 13:33:16 -0400

    Adding link in bio feature

---
+++ src/components/ui/linkInBio/MiniProfile.js
@@ -0,0 +1,31 @@
+import React from "react"
+import HeadshotImage from "../imageQueries/headshot"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faInstagramSquare,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons"
+
+const MiniProfile = () => (
+  <>
+    <div className="mini-profile">
+      <div className="mini-profile-img">
+        <HeadshotImage />
+      </div>
+      <span className="mini-profile-link">
+        <FontAwesomeIcon
+          icon={faInstagramSquare}
+          className="mini-profile-icon"
+        />{" "}
+        <a href="https://www.instagram.com/brandicodes"> @brandicodes</a>
+      </span>
+      <span className="mini-profile-link">
+        <FontAwesomeIcon icon={faTwitterSquare} className="mini-profile-icon" />{" "}
+        <a href="https://www.twitter.com/brandiCodes"> @brandiCodes</a>
+      </span>
+      <h3>Lover of coffee, coding, and my cat.</h3>
+    </div>
+  </>
+)
+
+export default MiniProfile
---
+++ src/components/ui/linkInBio/data.js
@@ -0,0 +1,36 @@
+export const Resources = [
+  {
+    title: "The Odin Project",
+    path: "https://www.theodinproject.com",
+  },
+  {
+    title: "freeCodeCamp",
+    path: "https://www.freecodecamp.org",
+  },
+  {
+    title: "codecademy",
+    path: "https://www.codecademy.com",
+  },
+  {
+    title: "Harvard CS50",
+    path: "https://cs50.harvard.edu/x/2021/",
+  },
+]
+
+export const OpenSource = [
+  {
+    title: "WeCanCode Club",
+    path: "https://github.com/WeCanCode-Club/WeCanCode",
+  },
+]
+
+export const BlogPosts = [
+  {
+    title: "4 Steps to Help You Overcome Imposter Syndrome",
+    path: "/blog/overcoming-imposter-syndrome",
+  },
+  {
+    title: "Learn to Code for as Little $$$ as Possible...",
+    path: "/blog/learning-to-code-on-a-budget",
+  },
+]
